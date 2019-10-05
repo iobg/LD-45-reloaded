f0187963cbe810ec6032c67ed956277424863fe3
iobg <iobg0320@gmail.com>
2019-10-04 23:24:04 -0400

    bronkedn

--- Assets/Scripts/FollowerController.cs
+++ Assets/Scripts/FollowerController.cs
@@ -2,13 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
-    public static PlayerController instance {get; private set;}
+public class FollowerController : MonoBehaviour
+{	
+	PlayerController player = PlayerController.instance;
 	public float moveSpeed;
-    public float horizontal;
-    public float vertical;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -18,10 +15,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector2 position = player.GetComponent<Rigidbody2D>().position;
         Vector2 move = new Vector2(horizontal, vertical);
-        Vector2 position = GetComponent<Rigidbody2D>().position;
         position = position + move * moveSpeed * Time.deltaTime;
         GetComponent<Rigidbody2D>().MovePosition(position);
     }
