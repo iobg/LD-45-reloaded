9929932b4b77208881b896719af78f9b8e151384
iobg <iobg0320@gmail.com>
2019-10-04 22:51:59 -0400

    We made a game!!!

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector2 move = new Vector2(horizontal, vertical);
+        Vector2 position = GetComponent<Rigidbody2D>().position;
+        position = position + move * moveSpeed * Time.deltaTime;
+        GetComponent<Rigidbody2D>().MovePosition(position);
+    }
+}
