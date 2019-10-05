962ad4920ea36ab33fd59c1b2b791c52d9fac417
iobg <iobg0320@gmail.com>
2019-10-05 00:44:22 -0400

    Followers lookin clean

--- Assets/Scripts/FollowerController.cs
+++ Assets/Scripts/FollowerController.cs
@@ -20,6 +20,33 @@ public class FollowerController : MonoBehaviour
     {   float vertical = PlayerController.instance.y + verticalOffset;
         float horizontal = PlayerController.instance.x + horizontalOffset;
         Vector2 move = new Vector2(horizontal, vertical);
+        position = GetComponent<Rigidbody2D>().position;
+
+        if(position.x + horizontalOffset > move.x + horizontalOffset){
+            move.x = position.x - (float)0.1;
+            if(move.x - position.x < 0.2 && move.x -position.x > 0 || move.x - position.x > - 0.2 && move.x -position.x < 0 ){
+                Debug.Log("CORRECTION");
+                move.x = PlayerController.instance.x + horizontalOffset;
+            }
+        }
+
+        if(position.x + horizontalOffset < move.x + horizontalOffset ){
+            move.x = position.x + (float)0.1;
+        }
+
+        if(position.y + verticalOffset > move.y + verticalOffset){
+            move.y = position.y - (float)0.1;            
+            if(move.y - position.y < 0.2 && move.y -position.y > 0 || move.y - position.y > - 0.2 && move.y -position.y < 0 ){
+                Debug.Log("CORRECTION");
+                move.y = PlayerController.instance.y + verticalOffset;
+            }
+        }
+
+
+        if(position.y + verticalOffset < move.y + verticalOffset ){
+            move.y = position.y + (float)0.1;
+        }
+
         GetComponent<Rigidbody2D>().MovePosition(move);
     }
 }
