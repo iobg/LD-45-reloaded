66b82cdb0d85baadbfdf6bb49d7cebb4fff8cfd7
BloonZhang <jeffrey.l.zhang@vanderbilt.edu>
2019-10-05 21:29:55 -0500

    pickaxe can pick things

---
+++ Assets/Scripts/FarmerController.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class FarmerController : MonoBehaviour
+{
+
+    // Singleton
+    public static FarmerController instance {get; private set;}
+
+    // Public variables
+    public float moveSpeed = 10;
+    public float actionSpeed = 0.5f;
+    public Sprite[] spriteArray; // nothing for fists, [0] for pickaxe, [1] for scythe
+
+    // Private variables
+    private string equippedObject;
+    private SpriteRenderer equippedSprite;
+    private float actionTimer;
+    private static TilemapController tilemap;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        // Equipped Objects
+        equippedObject = "fists";
+        equippedSprite = transform.Find("EquippedItem").gameObject.GetComponent<SpriteRenderer>();
+        equippedSprite.sprite = null;
+        tilemap = TilemapController.instance;
+    }
+
+    void Update()
+    {
+        // Movement
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector2 move = new Vector2(horizontal, vertical);
+        Vector2 position = GetComponent<Rigidbody2D>().position;
+        position = position + move * moveSpeed * Time.deltaTime;
+        GetComponent<Rigidbody2D>().MovePosition(position);
+
+        // Switching equipment
+        if (Input.GetKeyDown("1")){
+            equippedObject = "fists";
+            equippedSprite.sprite = null;
+        } else if (Input.GetKeyDown("2")){
+            equippedObject = "pickaxe";
+            equippedSprite.sprite = spriteArray[0];
+        } else if (Input.GetKeyDown("3")){
+            equippedObject = "scythe";
+            equippedSprite.sprite = spriteArray[1];
+        }
+
+        // On player click
+        if (Input.GetMouseButton(0) && actionTimer <= 0)
+        {
+            actionTimer = actionSpeed;
+            Vector3Int affectedTile = tilemap.getAffectedTile();
+            // Depending on what is equipped
+            switch(equippedObject)
+            {
+                case "fists":
+                    punchTile(affectedTile);
+                    break;
+                case "pickaxe":
+                    pickTile(affectedTile);
+                    break;
+                case "scythe":
+                    harvestTile(affectedTile);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
+        //Reduce timers
+        if (actionTimer > 0){
+            actionTimer -= Time.deltaTime;
+        }
+    }
+
+
+
+    // If player has fists
+    void punchTile(Vector3Int affectedTile){
+        return;
+    }
+
+
+    // If player has pickaxe
+    void pickTile(Vector3Int affectedTile){
+        tilemap.destroyTile(affectedTile);
+        return;
+    }
+
+    // If player has scythe
+    void harvestTile(Vector3Int affectedTile){
+        return;
+    }
+
+}
--- Assets/Scripts/TilemapController.cs
+++ Assets/Scripts/TilemapController.cs
@@ -5,33 +5,68 @@ using UnityEngine.Tilemaps;
 
 public class TilemapController : MonoBehaviour
 {
+    //Singleton
+    public static TilemapController instance {get; private set;}
 
     //Public variables
+    public TileBase hoverTile; // Tile for the hover visual
 
     //Private variables
-    private Tilemap map;
-    private Grid grid;
+    private static Tilemap hoverMap;   //TODO if time: don't use an entire Tilemap for the hover visual
+    private static Tilemap groundMap;
+    private static Grid grid;
+    private TileBase farmerTile;
+    private static FarmerController farmer;
 
+    void Awake()
+    {
+        instance = this;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        map = GetComponent<Tilemap>();
-        grid = map.layoutGrid;
+        hoverMap = GetComponent<Tilemap>();
+        grid = hoverMap.layoutGrid;
+        groundMap = grid.transform.Find("Tilemap").gameObject.GetComponent<Tilemap>();
+        farmer = FarmerController.instance;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // To make sure there's only one selected tile at a time
+        hoverMap.ClearAllTiles();
 
         // save the camera as public field if you using not the main camera
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         // get the collision point of the ray with the z = 0 plane
         Vector3 worldPoint = ray.GetPoint(-ray.origin.z / ray.direction.z);
         Vector3Int position = grid.WorldToCell(worldPoint);
+        // Visual indicator of the tile the mouse is hovering
+        hoverMap.SetTile(position, hoverTile);
+    }
 
-        //Debug.Log(position);
-        TileBase hoverTile = map.GetTile(position);
-        Debug.Log(hoverTile);
+    public Vector3Int getAffectedTile(){
+        // What tile is the farmer standing on?
+        Vector3Int farmerCoordinate = grid.WorldToCell(farmer.GetComponent<Rigidbody2D>().position);
+        farmerTile = groundMap.GetTile(farmerCoordinate);
+        // What direction is the mouse relative to the farmer?
+        Vector3 screenPosition = Camera.main.WorldToScreenPoint(farmer.GetComponent<Rigidbody2D>().position);
+        Vector3 mouseDirection = Input.mousePosition - new Vector3(screenPosition.x, screenPosition.y, 0);
+        // let's get the tile in that direction (adjacent to the farmer)
+        Vector3Int affectedCoordinate = farmerCoordinate;
+        if (Mathf.Abs(mouseDirection.x) > Mathf.Abs(mouseDirection.y)){
+            affectedCoordinate.x += (int)Mathf.Sign(mouseDirection.x);
+        }else{
+            affectedCoordinate.y += (int)Mathf.Sign(mouseDirection.y);
+        }
+        //Debug.Log(affectedCoordinate);
+
+        return affectedCoordinate;
+    }
+
+    public void destroyTile(Vector3Int tileCoordinate){
+        groundMap.SetTile(tileCoordinate, null);
     }
 }
