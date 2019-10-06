4cc7323afe9b5161db62461499318de9eb279ef8
iobg <iobg0320@gmail.com>
2019-10-06 11:58:08 -0400

    Enemy shooting + movement + collides with player and follower

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -6,31 +6,97 @@ public class EnemyController : MonoBehaviour
 
 
 {
-	public float moveSpeed = 0.5f;
-    public float maxHealth = 100;
+    // Public variables
+    public float moveSpeed = 0.5f;
+    public float maxHealth = 10;
+    public float delayBetweenShots = 2.0f;
+    public bool hasWeapon;
+    public GameObject bulletPrefab;
+    public float enemyDamage;
+    // ***** Look for death torque and death velocity
 
-    Vector3 aimDirection;
+    // Properties
+    //public float Health {get { return currentHealth; }}
+
+    // Private Variables
+    PlayerController player;
     float currentHealth;
-    // bool KOed = false;
-
-
-
+    float shotTimer = 0;
+    float stunTimer = 0;
+    bool stunned = false;
+    bool KOed = false;
+    float KOTimer = 0;
+    Vector3 aimDirection;
     // Start is called before the first frame update
     void Start()
     {
+        // Grab the player
+        GameObject[] playerArray = GameObject.FindGameObjectsWithTag("Player");
+        if (playerArray == null){
+            Debug.Log("Player array was null, weird.");
+            return;
+        }
+        // Should be only one player
+        player = playerArray[0].GetComponent<PlayerController>();
+
         currentHealth = maxHealth;
     }
 
     // Update is called once per frame
-    void Update()
+ void Update()
     {
+        // Player is dead
+        // if (!PlayerController.instance.isAlive()){return;}
+        // Alive code
+        if (!KOed){
+            
+            // Stun timer count down
+            if (stunned){
+                stunTimer -= Time.deltaTime;
+                if (stunTimer <= 0) {stunned = false;}
+            }
+            // Shot timer count down
+            if (shotTimer > 0){ shotTimer -= Time.deltaTime; }
+
+            // Fly and shoot towards the player if not stunned and has weapon
+            if (!stunned && hasWeapon){
+                // Aim
+                aimDirection = player.transform.position - this.transform.position;
+                float atan2 = Mathf.Atan2(aimDirection.y, aimDirection.x);
+                transform.rotation = Quaternion.Euler(0f, 0f, atan2 * Mathf.Rad2Deg);
+                // Move
+                Vector2 moveDirection = new Vector2(aimDirection.x, aimDirection.y); moveDirection.Normalize();
+                Vector2 position = GetComponent<Rigidbody2D>().position;
+                position = position + moveDirection * moveSpeed * Time.deltaTime;
+                GetComponent<Rigidbody2D>().MovePosition(position);
+                // Shoot
+                if (shotTimer <= 0){
+                    GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+                    ProjectileController projectile = bullet.GetComponent<ProjectileController>();
+                    projectile.Launch(new Vector2(aimDirection.x, aimDirection.y), enemyDamage);
+                    shotTimer = delayBetweenShots;
+                    // audioSource.Play();
+                } 
+            } 
+            // Spin really cute if no weapon
+            else if (!stunned && !hasWeapon){
+                if (!hasWeapon){
+                    transform.Rotate(0, 0, 100 * Time.deltaTime);
+                }
+            }
+        }
+        // Dead code
+        else
+        {
+            KOTimer += Time.deltaTime;
+           Destroy(gameObject);
+        }
         
     }
 
         public void Damage(float damage)
     {
         currentHealth -= damage;
-        Debug.Log(currentHealth);
         // KO if no health left
         if (currentHealth <= 0){
             Rigidbody2D rigidbody2d = GetComponent<Rigidbody2D>();
@@ -40,8 +106,14 @@ public class EnemyController : MonoBehaviour
             rigidbody2d.AddTorque(20); // ********** Death torque ********
             Vector2 knockbackDirection = new Vector2(-aimDirection.x, -aimDirection.y); knockbackDirection.Normalize();
             rigidbody2d.AddForce(knockbackDirection * 50); // ******** Death velocity ********
-            Destroy(gameObject);
-            // KOed = true;
+            // Destroy(gameObject);
+            KOed = true;
         }
+    }
+
+        public void Stun(float stunTime)
+    {
+        stunned = true;
+        stunTimer = stunTime;
     }
 }
--- Assets/Scripts/ProjectileController.cs
+++ Assets/Scripts/ProjectileController.cs
@@ -11,8 +11,7 @@ public class ProjectileController : MonoBehaviour
 	public float bulletSpeed = 300.0f; // Note that speed is affected by mass too
     public float bulletMass = 0.3f;
     public float bulletStun = 0.3f;
-    public bool friendlyBullet;
-
+    
     float bulletDamage;
     // Start is called before the first frame update
     void Start()
@@ -41,18 +40,29 @@ public class ProjectileController : MonoBehaviour
     }
 
         void OnTriggerEnter2D(Collider2D col)
-    {
-    	Debug.Log(col.name);
+    {	
+
         if(col.name == "Walls"){
         	 Destroy(gameObject);
         }
-        if(col.name == "Enemy"){
+        if(col.tag == "Enemy" && gameObject.tag == "friendlyBullet"){
       		Destroy(gameObject);
 
       		 EnemyController enemy = col.GetComponent<EnemyController>();
             if (enemy != null) {
+            	enemy.Stun((float)0.5);
                 enemy.Damage(bulletDamage);
             }
+        }
+
+        if(col.tag == "Follower" && gameObject.tag == "enemyBullet"){
+      		Destroy(gameObject);
+
+        }
+
+         if(col.tag == "Player" && gameObject.tag == "enemyBullet"){
+      		Destroy(gameObject);
+
         }
        
         // restart = true;
